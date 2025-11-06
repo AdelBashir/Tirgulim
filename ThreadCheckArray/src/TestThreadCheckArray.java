1972201d4e936fb80319c61b43d9fa66700c5c88
adilb <adilb@10.10.14.82>
2025-11-06 14:02:45 +0200

    Third commit

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,71 +1,64 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
-	public static void main(String[] args) {
-		try (Scanner input = new Scanner(System.in)) {
-			Thread thread1, thread2;
-			System.out.println("Enter array size");
-			int num  = input.nextInt();
-			int [] array = new int[num];
-			System.out.println("Enter numbers for array");
-			
-			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
-			
-			System.out.println("Enter number");
-			num = input.nextInt();
-			
-			SharedData sd = new SharedData(array, num);
-			
-			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
-			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
-			thread1.start();
-			thread2.start();
-			try 
-			{
-				thread1.join();
-				thread2.join();
-			} 
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-			if (!sd.getFlag())
-			{
-				System.out.println("Sorry");
-				return;
-			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
-			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
-				System.out.print(index + "    ");
-			System.out.println();
-			System.out.print("A:    ");
-			for (int index : sd.getArray())
-			{
-				System.out.print(index);
-				int counter = 5;
-				while (true)
-				{
-					index = index / 10;
-					counter--;
-					if (index == 0)
-						break;
-				}
-				for (int i = 0; i < counter; i++)
-					System.out.print(" ");
-			}
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            Thread thread1, thread2;
 
-			System.out.println();
-			System.out.print("C:    ");
-			for (boolean index : sd.getWinArray())
-			{
-				if (index)
-					System.out.print("1    ");
-				else
-					System.out.print("0    ");	
-			}
-		}
-	}
+            System.out.println("Enter array size");
+            int n = input.nextInt();
 
+            ArrayList<Integer> array = new ArrayList<>(n);
+            System.out.println("Enter numbers for array");
+            for (int i = 0; i < n; i++) {
+                array.add(input.nextInt());
+            }
+
+            System.out.println("Enter number");
+            int b = input.nextInt();
+
+            SharedData sd = new SharedData(array, b);
+
+            thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
+            thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
+            thread1.start();
+            thread2.start();
+
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (!sd.getFlag()) {
+                System.out.println("Sorry");
+                return;
+            }
+
+            System.out.println("Solution for b : " + sd.getB() + ", n = " + sd.getArray().size());
+
+            // הדפסת אינדקסים
+            System.out.print("I:    ");
+            for (int i = 0; i < sd.getArray().size(); i++) {
+                System.out.printf("%-5d", i);
+            }
+            System.out.println();
+
+            // הדפסת ערכים
+            System.out.print("A:    ");
+            for (int val : sd.getArray()) {
+                System.out.printf("%-5d", val);
+            }
+            System.out.println();
+
+            // הדפסת בחירות (winArray)
+            System.out.print("C:    ");
+            for (boolean chosen : sd.getWinArray()) {
+                System.out.print(chosen ? "1    " : "0    ");
+            }
+            System.out.println();
+        }
+    }
 }
